c8aba218094bcd5f0f3975018d7e7d10e444aa0c
rabaa-hasan <rabaa.reyad.hasan@gmail.com>
2021-02-16 20:30:08 +0200

    expend js file

--- js/app.js
+++ js/app.js
@@ -1,57 +1,129 @@
 'use strict';
 
-let userName = prompt('what is your name?');
-alert('Hello! '+ userName);
+let userName = prompt('what is your name?').toUpperCase();
+alert('Hello! ' + userName);
 //console.log(userName);
+
+let count = 0;
 
 
 let asac = prompt('Am I Studying at (ASAC)?');
-asac=asac.toLowerCase();
+asac = asac.toLowerCase();
 //console.log(asac.toLowerCase());
+
+while (asac !== 'yes' && asac !== 'y' && asac !== 'ye' && asac !== 'no' && asac !== 'n') {
+  asac = prompt('Am I Studying at (ASAC)?').toLowerCase();
+}
 
 if (asac === 'y' || asac === 'ye' || asac === 'yes') {
   alert('This is right I am Studying in Asac');
-}else if (asac === 'n' || asac === 'no'){
+  count++;
+} else if (asac === 'n' || asac === 'no') {
   alert('This is Wrong I am Studying in Asac');
 }
 
 
+
 let progrogramming = prompt('Do I love Programming?');
-progrogramming=progrogramming.toLowerCase();
+progrogramming = progrogramming.toLowerCase();
 //console.log(progrogramming.toLowerCase());
+
+while (progrogramming !== 'yes' && progrogramming !== 'y' && progrogramming !== 'ye' && progrogramming !== 'no' && progrogramming !== 'n') {
+  progrogramming = prompt('Do I love Programming?').toLowerCase();
+}
 
 if (progrogramming === 'y' || progrogramming === 'ye' || progrogramming === 'yes') {
   alert('This is right I Love Programming');
-}else if (progrogramming === 'n' || progrogramming === 'no'){
+  count++;
+} else if (progrogramming === 'n' || progrogramming === 'no') {
   alert('Why not? :/ ');
 }
 
 let major = prompt('Does Information Security & Networks my major at college?');
-major=major.toLowerCase();
+major = major.toLowerCase();
 //console.log(major);
+
+while (major !== 'yes' && major !== 'y' && major !== 'ye' && major !== 'no' && major !== 'n') {
+  major = prompt('Does Information Security & Networks my major at college?').toLowerCase();
+}
 
 if (major === 'y' || major === 'ye' || major === 'yes') {
   alert('This is right my major in College Was IS&N ');
-}else if (major === 'n' || major === 'no'){
+  count ++;
+} else if (major === 'n' || major === 'no') {
   alert('This is Wrong -.-');
 }
 
-let season = prompt('Does winter my favourite season?');
-season=season.toLowerCase();
+let season = prompt('Does Spring my favourite season?');
+season = season.toLowerCase();
 //console.log(season);
 
+while (season !== 'yes' && season !== 'y' && season !== 'ye' && season !== 'no' && season !== 'n') {
+  season = prompt('Does Spring my favourite season?').toLowerCase();
+}
+
 if (season === 'y' || season === 'ye' || season === 'yes') {
+  alert('This is Wrong -.- \n winter is my favourite season ');
+} else if (season === 'n' || season === 'no') {
   alert('Right Winter is my Fav Season :)');
-}else if (season === 'n' || season === 'no'){
-  alert('This is Wrong -.-');
+  count++;
 }
 
 let parrot = prompt(' Do I have a parrot?');
-parrot=parrot.toLowerCase();
+parrot = parrot.toLowerCase();
 //console.log(parrot);
+
+while (parrot !== 'yes' && parrot !== 'y' && parrot !== 'ye' && parrot !== 'no' && parrot !== 'n') {
+  parrot = prompt(' Do I have a parrot?').toLowerCase();
+}
 
 if (parrot === 'y' || parrot === 'ye' || parrot === 'yes') {
   alert('This is right I have one :D ');
-}else if (parrot === 'n' || parrot === 'no'){
+  count++;
+} else if (parrot === 'n' || parrot === 'no') {
   alert('This is Wrong I do have one ');
 }
+
+
+let age = Number(prompt('How old am I ?'));
+//console.log(age);
+
+while (age === ' ' && age === null) {
+  age = Number(prompt('How old am I ?'));
+}
+
+for (let i = 0; i <= 3 ; i++) {
+  if (age > 20) {
+    alert('you did not guess it ☹️   \n  This is too high  Try again');
+    age = Number(prompt('How old am I ?'));
+  }
+  if (age < 20) {
+    alert('you did not guess it ☹️   \n This is too low  Try again');
+    age = Number(prompt('How old am I ?'));
+  }
+  if (age === 20) {
+
+    alert('This is right you got it 😄 \n I am 20 years old ');
+    count++;
+    { break; }
+  }
+
+}
+let arr0=[ 'orange' , 'watermelon' , 'apple' , 'peach '];
+let fruit=prompt('What is my favourite fruit?').toLowerCase();
+//console.log(arr0);
+
+for(let i=0; i<=5 ;i++){
+  if (fruit === arr0[0]){
+    alert('Great 😄 \n Orange is my favourite fruit ');
+    count++;
+    {break;}
+  }
+  else{
+    alert('you did not guess it ☹️   \n  Try again ');
+    fruit=prompt('What is my favourite fruit?').toLowerCase();
+  }
+}
+alert ('your score is : ' + count + '  of  7 ');
+
+
